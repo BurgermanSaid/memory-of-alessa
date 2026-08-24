727525bcfc3b02c0c6283d4c70f1ad71df9d6783
dreamingmoths <mothdreams666@gmail.com>
2026-08-24 16:17:58 -0700

    sh2: movie 🎬 (#321)
    * sh2: begin `pss_main` 🎬
    
    * match more `pss_main` functions
    
    * split `pss_main` rodata
    
    * add `pss_main` matches
    
    * sh2: add `pss_disp` matches
    
    * sh2: match `pss_audiodec`
    
    * update `sifdev.h`

--- include/sce/eekernel.h
+++ include/sce/eekernel.h
@@ -3,6 +3,14 @@
 
 #include "common.h"
 
+#define DMAC_VIF0               0
+#define DMAC_VIF1               1
+#define DMAC_GIF                2
+#define DMAC_FROM_IPU           3
+#define DMAC_TO_IPU             4
+#define DMAC_FROM_SPR           8
+#define DMAC_TO_SPR             9
+
 #define ExitHandler()           __asm__ volatile("sync.l; ei")
 
 typedef struct
@@ -14,6 +22,21 @@ typedef struct
     u_int attr;
     u_int option;
 } SemaParam;
+
+typedef struct ThreadParam {
+    int     status;
+    void    (*entry)(void *);
+    void    *stack;
+    int     stackSize;
+    void    *gpReg;
+    int     initPriority;
+    int     currentPriority;
+    u_int   attr;
+    u_int   option;
+    int     waitType;
+    int     waitId;
+    int     wakeupCount;
+} ThreadParam;
 
 int CreateSema(SemaParam* param);
 int SignalSema(int);
